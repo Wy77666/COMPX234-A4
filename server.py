80525ac6f946d91824cbcebf195f1096df6a98a5
Wy77666 <3177983314@qq.com>
2025-06-14 18:04:13 +0800

    Update server.py

--- server.py
+++ server.py
@@ -21,3 +21,23 @@ def handle_client_request(client_address, filename, server_socket):
     # Create a new thread to handle file transmission
     threading.Thread(target=handle_file_transmission, args=(filename, client_address, port)).start()
 
+# Function to handle file transmission
+def handle_file_transmission(filename, client_address, port):
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as file_socket:
+        file_socket.bind(("localhost", port))
+        with open(filename, "rb") as file:
+            while True:
+                request, _ = file_socket.recvfrom(1024)
+                request_parts = request.decode().split(" ")
+                if request_parts[0] == "FILE" and request_parts[1] == filename and request_parts[2] == "GET":
+                    start = int(request_parts[4])
+                    end = int(request_parts[6])
+                    file.seek(start)
+                    data = file.read(end - start + 1)
+                    encoded_data = base64.b64encode(data).decode()
+                    response = f"FILE {filename} OK START {start} END {end} DATA {encoded_data}"
+                    file_socket.sendto(response.encode(), client_address)
+                elif request_parts[0] == "FILE" and request_parts[1] == filename and request_parts[2] == "CLOSE":
+                    response = f"FILE {filename} CLOSE_OK"
+                    file_socket.sendto(response.encode(), client_address)
+                    break
